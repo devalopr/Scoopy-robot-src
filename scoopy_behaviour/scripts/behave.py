79feb49f9e6848c705e41883e80400992641b008
DevadathPR <prdevadathk3@gmail.com>
2021-09-06 07:02:46 -0700

    Updating python script

--- scoopy_behaviour/scripts/behave.py
+++ scoopy_behaviour/scripts/behave.py
@@ -7,6 +7,8 @@ import actionlib
 
 import cv2
 from std_msgs.msg import String
+from std_msgs.msg import Float64
+
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -20,7 +22,7 @@ class Behaviour:
     #Subscribing and publishing init
     def __init__(self) -> None:
 
-
+        #Keeping location points
         self.way_points = {}
 
         self.way_points["sink_pose"] = [-2.837,-1.963,-3.124]
@@ -31,21 +33,62 @@ class Behaviour:
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.client.wait_for_server()
         self.goal = MoveBaseGoal()
+
+        #Defining publishers
+        self.topic_name = {}
+        self.topic_name["camera"] = "/scooopy/camera1/image_raw"
+        self.topic_name["blw"] = "/scoopy/blw_revolute_position_controller/command"
+        self.topic_name["brw"] = "/scoopy/brw_revolute_position_controller/command"
+        #self.topic_name["camera_pan"] = "/scooopy/camera1/image_raw"
+        #self.topic_name["camera_tilt"] = "/scooopy/camera1/image_raw"
+        self.topic_name["lid"] = "/scoopy/lid_revolute_position_controller/command"
+        self.topic_name["mid_arm"] = "/scoopy/mid_inner_slider_position_controller/command"
+        self.topic_name["outer_arm"] = "/scoopy/outer_mid_slider_position_controller/command"
+        self.topic_name["post_slider"] = "/scoopy/post_slider_position_controller/command"
+        self.topic_name["tool_head"] = "/scoopy/toolhead_revolute_position_controller/command"
+
+
+
+        self.post_joint = rospy.Publisher(self.topic_name["post_slider"],Float64,queue_size=10)
+        self.outer_joint = rospy.Publisher(self.topic_name["outer_arm"],Float64,queue_size=10)
+        self.mid_joint = rospy.Publisher(self.topic_name["mid_arm"],Float64,queue_size=10)
+        self.lid_joint = rospy.Publisher(self.topic_name["lid"],Float64,queue_size=10)
+        self.tool_head_joint = rospy.Publisher(self.topic_name["tool_head"],Float64,queue_size=10)
+        
+        #self.post_joint = rospy.Publisher(self.topic_name["post_slider"],Float64,queue_size=10)
+        #self.post_joint = rospy.Publisher(self.topic_name["post_slider"],Float64,queue_size=10)
+
+        #pub = rospy.Publisher('topic_name', std_msgs.msg.String, queue_size=10)
+
+
         #pass
         self.behave()
 
 
     def behave(self):
+        rospy.loginfo("Moving near to sink")
         self.move_location("sink_pose")
-        self.move_location("center_pose")
-        self.move_location("exit_pose")
+        rospy.loginfo("Completed sink pose movement")
+        rospy.sleep(2)
+        rospy.loginfo("Moving post slider up")
+        self.move_joint("post_slider", 1)
+        rospy.sleep(15)
+        rospy.loginf0("Completed post slider movement")
 
-        #pass
+
+        #self.move_location("center_pose")
+        #self.move_location("exit_pose")
+
+    def init_pose(self):
+        rospy.loginfo("Moving to inital arm configuration")
+        self.move_joint("post_slider", 1)
+
 
 
     def move_location(self,location_name):
+        rospy.loginfo("Moving into: "+location_name)
+
         map_coord = self.way_points[location_name]
-        #print (self.way_points['sink_pose'])
         self.send_movebase_pose(map_coord[0],map_coord[1],map_coord[2])
 
     #Sending goal to move base
@@ -74,11 +117,24 @@ class Behaviour:
 
 
 
-        pass
 
     #Moving each joint
-    def move_joint(self,joint_val):
-        pass
+    def move_joint(self,name,joint_val):
+
+        if(name == "post_slider"):
+            self.post_joint.publish(joint_val)
+
+        elif(name == "outer_arm"):
+            self.outer_joint.publish(joint_val)
+
+        elif(name == "mid_arm"):
+            self.mid_joint.publish(joint_val)
+
+        elif(name == "lid"):
+            self.lid_joint.publish(joint_val)
+
+        elif(name == "tool_head"):
+            self.tool_head_joint.publish(joint_val)
 
     #Return centroid of the detected color
     def detect_object(self,color):
